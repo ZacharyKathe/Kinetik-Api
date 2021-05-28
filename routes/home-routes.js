d3da7f67dcb85e5ea2c5254e2085f994aeb423d5
tylercmac <tylercmac@gmail.com>
2021-05-28 12:15:11 -0700

    fixed missing await

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -11,7 +11,7 @@ const { User, Goal, Group, Comment } = require('../models')
 router.get("/dashboard", tokenAuth, async (req, res) => {
   try {
     console.log(req.user.id);
-    const loggedUser = User.findOne({
+    const loggedUser = await User.findOne({
       where: {
         id: req.user.id
       },
@@ -20,7 +20,7 @@ router.get("/dashboard", tokenAuth, async (req, res) => {
         include: [{ model: Comment }]
       }, { model: Group }]
     })
-
+    console.log(loggedUser);
 
     res.status(200).json(loggedUser)
   } catch (err) {
